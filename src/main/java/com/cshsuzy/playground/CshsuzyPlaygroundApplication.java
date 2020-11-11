c090d6715555955bc5fc98ef87e8f5a866949d59
PandaRory <m88506@gmail.com>
2020-11-11 21:26:56 +0800

    Initial commit

---
+++ src/main/java/com/cshsuzy/playground/CshsuzyPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.cshsuzy.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CshsuzyPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CshsuzyPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cshsuzy/playground/CshsuzyPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cshsuzy.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CshsuzyPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
