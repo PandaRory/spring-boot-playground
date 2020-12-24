164585b4ef076a6e863685ce8c8edd16af612b80
PandaRory <m88506@gmail.com>
2020-12-25 00:21:15 +0800

    Add swagger ui

--- src/main/java/com/cshsuzy/playground/web/controller/UserController.java
+++ src/main/java/com/cshsuzy/playground/web/controller/UserController.java
@@ -2,19 +2,20 @@ package com.cshsuzy.playground.web.controller;
 
 import com.cshsuzy.playground.UserRepository;
 import com.cshsuzy.playground.web.entity.UserEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.text.html.parser.Entity;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/api")
+@Api(tags="使用者管理")
 public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
@@ -22,11 +23,13 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    @RequestMapping("/users")
-    public String users() {
-        for (UserEntity user : userRepository.findAll()) {
-            log.info(user.toString());
-        }
-        return "Finish";
+    @ApiOperation(value = "取得使用者", notes = "列出所有使用者")
+    @GetMapping("/users")
+    public List<UserEntity> users() {
+        List<UserEntity> userEntityList =  userRepository.findAll();
+//        for (UserEntity user : userEntityList) {
+//            log.info(user.toString());
+//        }
+        return userEntityList;
     }
 }
--- src/main/java/com/cshsuzy/playground/web/entity/UserEntity.java
+++ src/main/java/com/cshsuzy/playground/web/entity/UserEntity.java
@@ -1,16 +1,25 @@
 package com.cshsuzy.playground.web.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "user")
+@ApiModel("使用者基本資訊")
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @ApiModelProperty("ID")
     private int id;
+
+    @ApiModelProperty("EMAIL")
     @Column(name = "EMAIL")
     private String email;
+
+    @ApiModelProperty("手機")
     @Column(name = "PHONE")
     private String phone;
 
